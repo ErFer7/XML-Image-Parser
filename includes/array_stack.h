51c41fbcbce9bc9170af837c1e3de2721eec240c
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2021-08-10 16:09:49 -0300

    v0.1: Detecção de erros

---
+++ includes/array_stack.h
@@ -0,0 +1,142 @@
+// Copyright [2021] <Eric Fernandes Evaristo>
+#ifndef STRUCTURES_ARRAY_STACK_H
+#define STRUCTURES_ARRAY_STACK_H
+
+#include <cstdint>   // std::size_t
+#include <stdexcept> // C++ exceptions
+
+namespace structures
+{
+
+    template <typename T>
+    //! CLASSE PILHA
+    class ArrayStack
+    {
+    public:
+        //! construtor simples
+        ArrayStack();
+        //! construtor com parametro tamanho
+        explicit ArrayStack(std::size_t max);
+        //! destrutor
+        ~ArrayStack();
+        //! metodo empilha
+        void push(const T &data);
+        //! metodo desempilha
+        T pop();
+        //! metodo retorna o topo
+        T &top();
+        //! metodo limpa pilha
+        void clear();
+        //! metodo retorna tamanho
+        std::size_t size();
+        //! metodo retorna capacidade maxima
+        std::size_t max_size();
+        //! verifica se esta vazia
+        bool empty();
+        //! verifica se esta cheia
+        bool full();
+
+    private:
+        T *contents;
+        int top_;
+        std::size_t max_size_;
+
+        static const auto DEFAULT_SIZE = 10u;
+    };
+
+} // namespace structures
+
+#endif
+
+template <typename T>
+structures::ArrayStack<T>::ArrayStack()
+{
+    max_size_ = DEFAULT_SIZE;
+    contents = new T[max_size_];
+    top_ = -1;
+}
+
+template <typename T>
+structures::ArrayStack<T>::ArrayStack(std::size_t max)
+{
+    max_size_ = max;
+    contents = new T[max_size_];
+    top_ = -1;
+}
+
+template <typename T>
+structures::ArrayStack<T>::~ArrayStack()
+{
+    delete[] contents;
+}
+
+template <typename T>
+void structures::ArrayStack<T>::push(const T &data)
+{
+    if (full())
+    {
+        throw std::out_of_range("pilha cheia");
+    }
+    else
+    {
+        contents[++top_] = data;
+    }
+}
+
+template <typename T>
+T structures::ArrayStack<T>::pop()
+{
+    if (empty())
+    {
+        throw std::out_of_range("pilha vazia");
+    }
+    else
+    {
+        T temp;
+        temp = contents[top_--];
+        return temp;
+    }
+}
+
+template <typename T>
+T &structures::ArrayStack<T>::top()
+{
+    if (empty())
+    {
+        throw std::out_of_range("pilha vazia");
+    }
+    else
+    {
+        return contents[top_];
+    }
+}
+
+template <typename T>
+void structures::ArrayStack<T>::clear()
+{
+    top_ = -1;
+}
+
+template <typename T>
+std::size_t structures::ArrayStack<T>::size()
+{
+    return top_ + 1;
+}
+
+template <typename T>
+std::size_t structures::ArrayStack<T>::max_size()
+{
+    return max_size_;
+}
+
+template <typename T>
+bool structures::ArrayStack<T>::empty()
+{
+    return top_ == -1;
+}
+
+template <typename T>
+bool structures::ArrayStack<T>::full()
+{
+    return top_ == static_cast<int>(max_size() - 1);
+}
--- main.cpp
+++ main.cpp
@@ -1,18 +1,82 @@
 #include <iostream>
+#include <fstream>
+#include <array_stack.h>
+
+// v0.1
 
 int main() {
 
-    char xmlfilename[100];
+    char xmlFileName[100];
 
-    std::cin >> xmlfilename;  // entrada
-    
-    /*
-     
-       COLOQUE SEU CODIGO AQUI
-    
-    */
+    std::cin >> xmlFileName;  // entrada
 
-    std::cout << xmlfilename << std::endl;  // esta linha deve ser removida
+    std::ifstream xmlFileStream;
+    std::string xmlFile;
+    std::string line;
+
+    xmlFileStream.open(xmlFileName);
+    
+    while (getline(xmlFileStream, line)) {
+        xmlFile.append(line);
+    }
+
+    std::string tag;
+    structures::ArrayStack<std::string> tags;
+
+    bool readingTag = false;
+    bool openingTag = false;
+    bool tagIsFree = true;
+    bool valid = true;
+
+    for (int i = 0; i < xmlFile.length(); ++i) {
+
+        if (xmlFile[i] == '<') {
+            readingTag = true;
+            tagIsFree = false;
+
+            if (xmlFile[i + 1] == '/') {
+                openingTag = false;
+                i += 2;
+            } else {
+                openingTag = true;
+                ++i;
+            }
+        } else if (xmlFile[i] == '>') {
+            readingTag = false;
+        }
+
+        if (readingTag) {
+            tag.push_back(xmlFile[i]);
+        } else if (!tagIsFree) {
+
+            if (openingTag) {
+                tags.push(tag);
+            } else {
+                if (tags.top() == tag) {
+                    tags.pop();
+                } else {
+                    valid = false;
+                    break;
+                }
+            }
+            tag.clear();
+            tagIsFree = true;
+        }
+    }
+
+    if (!tags.empty()) {
+        valid = false;
+    }
+
+    // for (int i = 0; i < tags.size(); ++i) {
+    //     std::cout << tags.pop() << std::endl;
+    // }
+
+    if (valid) {
+        std::cout << "ok" << std::endl;
+    } else {
+        std::cout << "error" << std::endl;
+    }
 
     return 0;
 }
